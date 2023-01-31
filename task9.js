30544d92e1db93b8f3241aa6e88dae7f045c2860
priti73 <pritikumarinits@gmail.com>
2023-01-31 15:17:50 +0530

    task2

--- task9.js
+++ task9.js
@@ -1,7 +1,12 @@
 
-const http=require('http');
-const routes=require('./routes');
-console.log(routes.sometext);
-const server=http.createServer(routes.handler);
-
-server.listen(4000);
+const express=require('express');
+const app=express();
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    res.send('<h1>Hello from express!</h1>');
+});
+app.listen(4000);
