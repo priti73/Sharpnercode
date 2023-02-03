48941df1862c5988656f1dead503116a35634043
priti73 <pritikumarinits@gmail.com>
2023-02-03 18:34:08 +0530

    task8

--- controller/product.js
+++ controller/product.js
@@ -1,17 +1,21 @@
 const path=require('path');
 const express=require('express');
 const rootDir=require('../util/path');
-const product=[];
+const Product=require('../models/product');
+
 exports.getAddproduct=(req,res,next)=>{
     res.sendFile(path.join(rootDir,'views','admin.html'));
 }
 exports.postAddProduct=(req,res,next)=>{
     console.log(req.body.title);
     console.log(req.body.sizeofproduct);
-    product.push({title: req.body.title});
-
+    const product=new Product(req.body.title);
+    product.save();
     res.redirect('/shop');
 }
 exports.getProduct=(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','shop.html'));
+    Product.fetchAll((products)=>{
+        res.sendFile(path.join(rootDir,'views','shop.html')); 
+    });
+    //res.sendFile(path.join(rootDir,'views','shop.html'));
 }
---
+++ models/product.js
@@ -0,0 +1,38 @@
+const fs=require('fs');
+const path=require('path');
+const p=path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'    
+);
+
+const getproductsFromFile=cb =>{
+  fs.readFile(p,(err,fileContent)=>{
+    if(err){
+      cb([]);
+    }
+    else{
+      cb(JSON.parse(fileContent));
+    }
+});
+}
+
+module.exports=class Product{
+    constructor(t){
+        this.title=t;
+    }
+
+     save(){
+        getproductsFromFile(products=>{
+          products.push(this);
+          fs.writeFile(p,JSON.stringify(products), (err)=>{
+            console.log(err);
+          });
+        });   
+     }
+
+
+      static fetchAll(cb){
+        getproductsFromFile(cb);
+    }
+};
